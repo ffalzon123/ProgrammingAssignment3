e99d3034cd7068c2b9436aee4c64188350308233
ffalzon123 <ffalzon@hotmail.com>
2015-07-27 19:50:16 +0930

    added validation

--- best.R
+++ best.R
@@ -1,10 +1,27 @@
 best <- function(state, outcome) {
-  
-  ## Read outcome data
-  adf
-  ## Check that state and outcome are valid
-  
-  ## Return hospital name in that state with lowest 30-day death
-  ## rate
-  
+    ## Read outcome data
+    outcomeData <-
+        read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    
+    ## Check that state and outcome are valid
+    
+    vectorOfStates <- unique(outcomeData[, 7])
+    vectorOfOutcomes <- c("heart attack","heart failure","pneumonia")
+    validState <- state %in% vectorOfStates
+    validOutcome <- outcome %in% vectorOfOutcomes
+
+    if (validState) {
+        if (validOutcome) {
+            print("do stuff")
+        } else {
+            print("invalid outcome")
+        }
+        
+    } else {
+        print("invalid state")
+    }
+    
+    ## Return hospital name in that state with lowest 30-day death
+    ## rate
+    
 }
