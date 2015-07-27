95af6c6b8c4fdcb284723ceded2b09aa479239b8
ffalzon123 <ffalzon@hotmail.com>
2015-07-27 16:11:53 +0930

    moved files

--- best.R
+++ best.R
@@ -1,7 +1,7 @@
 best <- function(state, outcome) {
   
   ## Read outcome data
-  
+  adf
   ## Check that state and outcome are valid
   
   ## Return hospital name in that state with lowest 30-day death
