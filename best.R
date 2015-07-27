6044b511645a374c8ebe14952c9a50383309bf45
ffalzon123 <ffalzon@hotmail.com>
2015-07-27 22:22:19 +0930

    working for heart attack

--- best.R
+++ best.R
@@ -12,7 +12,27 @@ best <- function(state, outcome) {
 
     if (validState) {
         if (validOutcome) {
-            print("do stuff")
+            
+            ## filter by state
+            StateData <- outcomeData[outcomeData$State == state,]
+            ## convert rates to numeric
+            suppressWarnings(StateData[, 11]<- as.numeric(StateData[, 11]))
+            suppressWarnings(StateData[, 17]<- as.numeric(StateData[, 17]))
+            suppressWarnings(StateData[, 23]<- as.numeric(StateData[, 23]))
+            
+            rates <- StateData[, 11] ## vector of rate
+            minRate <- min(rates, na.rm = TRUE) ## min rate for outcome
+            bestHospital <- StateData[c(2,11)]
+            bestHospital <- bestHospital[complete.cases(bestHospital),]
+            names(bestHospital) <- c("Hospital.Name","Rate")
+            bestHospital <- bestHospital[bestHospital$Rate == minRate,]
+            
+            ## order by hospital name
+            bestHospital <- bestHospital[with(bestHospital, order(Hospital.Name)),]
+            
+
+            return(bestHospital[1,1])
+            
         } else {
             print("invalid outcome")
         }
